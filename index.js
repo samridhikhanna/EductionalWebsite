7391b7c26cd50f95398016bf9bb2262b8cda20b0
Samridhi <samridhikhanna2206@gmail.com>
2022-05-02 12:35:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//Navigation bar
+
+let nav=document.querySelector("nav");
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+    }
+    else{
+        nav.classList.remove("sticky");
+    }
+}
